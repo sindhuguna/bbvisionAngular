4c76817c59d1a6271cefb66ed248834b3b2ac109
Jaikumar <jaikumarhere007@gmail.com>
2021-08-20 14:18:13 +0530

    testing sindhu

--- src/app/master/rolemaster/rolemaster.component.ts
+++ src/app/master/rolemaster/rolemaster.component.ts
@@ -4,7 +4,7 @@ import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { RoleMaster } from '../model/rolemaster';
-
+//hh
 @Component({
   selector: 'app-rolemaster',
   templateUrl: './rolemaster.component.html',
